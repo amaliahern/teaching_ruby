675fc0d009ba6e7fdf7ea0129f1e75d81a1025a6
amaliahern <amaliahern@gmail.com>
2012-03-15 10:58:52 +0100

    exercises 1 & exercises 2

--- w2_array2/array2_spec.rb
+++ w2_array2/array2_spec.rb
@@ -1,24 +1,25 @@
-$: << File.expand_path(File.dirname(__FILE__))
+  $: << File.expand_path(File.dirname(__FILE__))
 
-require 'array2'
+  require 'array2'
 
 describe Array2 do
 
+  let(:array2) { Array2.new(1, 'a', 2, 'b', 3, 'c', 4, 8) }
+
   describe '::new' do
     it 'is created with a set of elements' do
-      Array2.new(1, 2, 3).elements.should eq [1, 2, 3]
+      #Array2.new(1, 2, 3).elements.should eq [1, 2, 3]
+       Array2.new(1, 2, 3).elements.should eq [1, 2, 3]
     end
 
     it 'is created with no elements if none specified' do
       Array2.new.elements.should eq []
     end
 
-    it 'handles any number of elements' do
+   it 'handles any number of elements' do
       Array2.new(1, 2, 3, 4, 5).elements.should eq [1, 2, 3, 4, 5]
     end
   end
-
-  let(:array2) { Array2.new(1, 'a', 2, 'b', 3, 'c', 4, 8) }
 
   describe '#length' do
     it 'returns the length of the array' do
@@ -48,6 +49,7 @@ describe Array2 do
     it 'shifts the elements from index to the last one to the right' do
       array2.at(6).should eq 4
       array2.at(8).should eq 8
+      array2.at(7).should eq 'new element'
     end
 
     it 'returns self' do
@@ -61,13 +63,124 @@ describe Array2 do
       array2.at(0).should eq 'a'
       array2.length.should eq 7
     end
-
+    
     it 'returns the element if there was an element with that index' do
       array2.delete_at(0).should eq 1
     end
 
     it 'returns nil if ther was no element with that index 'do
       array2.delete_at(99).should eq nil
+    end
+
+  end
+
+  describe '#empty' do
+    
+    it 'returns false if there are elements' do
+      array2.empty.should eq false
+    end
+
+    it 'returns true if no elements' do
+      array2 = Array2.new()
+      array2.empty.should eq true
+    end
+
+  end 
+
+  describe  '#include' do
+
+    it 'return true if element given is in array' do
+      array2.include('a').should eq true 
+    end
+
+    it 'return false if element given is not in array' do
+      array2.include('d').should eq false
+    end
+  end
+
+  describe '#index' do
+
+    it 'return index if element given is in array' do
+      array2.at(3).should eq 'b'
+      array2.index('b').should eq 3 
+    end
+
+    it 'return nil if element given is not in array' do
+      array2.index('z').should eq nil 
+    end
+  end
+
+  describe '#clear' do
+    it 'deletes all elements if array has got some elements' do
+      array2.clear()
+      array2.empty.should eq true
+    end
+
+   # it 'return nil if array is empty' do
+    # array2 = Array2.new()
+    #  array2.clear().should eq nil
+   # end
+    #
+  end
+  
+
+  describe '#delete' do
+    it 'deletes an element if array contains it' do
+      array2.at(1).should eq 'a'
+      array2.length.should eq 8
+      array2.delete('a')
+      array2.length.should eq 7
+      array2.at(1).should eq 2
+    end
+
+    it 'returns nil if no exists element in array' do
+      array2.delete('z').should eq nil
+    end
+  end
+
+
+  describe '#concat' do
+    it 'returns original array after append it in other object array' do
+      other_array2 = Array2.new('z',9)
+      array2.concat(other_array2).should eq array2
+      array2.elements.should eq [1,'a',2,'b',3,'c',4,8,'z',9]
+    end
+  end
+
+
+  describe '#drop' do
+    it 'return original array without the first n elements have been deleted' do
+      array2.drop(4).should eq array2
+      array2.length.should eq 4
+      array2.elements.should eq [3,'c',4,8]
+    end
+  end
+
+  describe '#compact' do
+  let(:array3) { Array2.new(nil, nil, nil, 4) }
+     
+    it 'return array without nil elements' do
+      array3.compact
+      array3.length.should eq 1 
+      array3.elements.should eq [4]
+    end
+  end
+
+  describe '#uniq' do
+  let(:array2) { Array2.new(1,1,1,1) }
+    it 'return new array without duplicate elements' do
+      array2.uniq
+      array2.length.should eq 1 
+      array2.elements.should eq [1]
+    end
+  end
+
+  describe '#reverse' do
+    let(:reverse_array){Array2.new()}
+    it 'return new array with elements in inverse orden' do
+      array2.reverse(reverse_array)
+      array2.length.should eq 8
+      array2.elements.should eq [8, 4,'c' ,3, 'b',2 ,'a', 1]
     end
   end
 
